794bd3da5cf19b2c03cddd0352bc8dfb49d647a5
Julio Cesar Castro Macias <83622621+lmEkoz@users.noreply.github.com>
2022-03-17 20:40:43 -0600

    Add files via upload

--- dsd/mux.vhd
+++ dsd/mux.vhd
@@ -3,24 +3,13 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity mux is
-    port(
-        a       : in std_logic_vector(3 downto 0);
-        b       : in std_logic_vector(3 downto 0);
-        c       : in std_logic_vector(3 downto 0);
-        sel     : in std_logic_vector(1 downto 0);
-        salida  : out std_logic_vector(3 downto 0);
-    )
+    port(s0,s1,d0,d1,d2,d3 : in bit; y : out bit);
 end mux;
 
-architecture synth of mux is    
+architecture data of mux is    
 begin
-    process(sel, a, b, c) is
-        case sel is
-            when "00" => salida <= (others => '0';)
-            when "01" => salida <= a;
-            when "10" => salida <= b;
-            when "11" => salida <= c;
-            when others => salida <= (others => '0');
-        end case;
-    end process
-end architecture synth;
+    y<= (not s0 and not s1 and d0) or
+        (s0 and not s1 and d1) or
+        (not s0 and s1 and d2) or
+        (s0 and s1 and d3);
+end architecture data;
